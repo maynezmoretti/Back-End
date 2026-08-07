4cb2d59013b788fefa61b614e880b385a656b1aa
maynezmoretti <maynezardo@gmail.com>
2026-08-07 09:35:26 -0300

    Index

---
+++ PHPVanilla/Estudo_Variáveis/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo de Variáveis</title>
+</head>
+<body>
+    <h3>Estudo de Variáveis</h3>
+    <?php
+    // sintaxe de variáveis em PHP
+    // variáveis são representadas pelo símbolo "$" seguido do nome da variável
+    // exemplo:
+    $nome = "Mayne"; // variável do tipo String
+    ?>
+    
+</body>
+</html>
